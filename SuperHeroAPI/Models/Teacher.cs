c2ca9da7e424f3f5b6027528ff809d2090a18797
Malkollm <malkollm.mob.projects@gmail.com>
2022-12-25 23:58:17 +0500

    Добавил миграции на таблицу Учителей

---
+++ SuperHeroAPI/Models/Teacher.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroAPI.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }        
+        public string LastName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public string Lesson { get; set; } = string.Empty;
+    }
+}
